2384164861ed3c0faecdfaeef75c05a8caa7cd70
oliver <o.kohl@sap.com>
2023-11-28 11:53:53 +0100

    simplify

--- main.js
+++ main.js
@@ -4,16 +4,4 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import '@ui5/webcomponents/dist/Carousel.js'
 
-document.querySelector('#app').innerHTML = `
-<style>
-img {
-    max-height: 30rem;
-}
-</style>
-<ui5-carousel cyclic>
-<img src="/assets/sample1.jpg" alt="Landscape 1">
-<img src="/assets/sample2.jpg" alt="Landscape 2">
-<img src="/assets/sample3.jpg" alt="Bulb">
-</ui5-carousel>
-`
 
