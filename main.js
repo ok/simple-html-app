101cac0158d6e414a6f85fac20d714583be685cb
Ben Peter <benp@adobe.com>
2023-11-28 22:18:25 +0100

    use card, not carousel

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import '@ui5/webcomponents/dist/Carousel.js'
+import '@ui5/webcomponents/dist/Card.js'
 
 
