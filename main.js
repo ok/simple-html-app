a1fcf33b6a36f3aafeb2b7242abac39c759d6fcb
oliver <o.kohl@sap.com>
2023-11-28 11:46:34 +0100

    carousel and images added

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import '@ui5/webcomponents/dist/Button.js'
+import '@ui5/webcomponents/dist/Carousel.js'
 
 document.querySelector('#app').innerHTML = `
-  <ui5-button>Hello UI5 Web Components</ui5-button>
+<style>
+img {
+    max-height: 30rem;
+}
+</style>
+<ui5-carousel cyclic>
+<img src="/assets/sample1.jpg" alt="Landscape 1">
+<img src="/assets/sample2.jpg" alt="Landscape 2">
+<img src="/assets/sample3.jpg" alt="Bulb">
+</ui5-carousel>
 `
 
