fe12e7ff6a381035d392316917fb0cfdd0e19a13
oliver <o.kohl@sap.com>
2023-11-29 16:56:57 +0100

    theme should load now with correct CSS

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme";
 import "@ui5/webcomponents-theming/dist/Assets.js"
 import "@ui5/webcomponents-icons/dist/AllIcons.js"
 import '@ui5/webcomponents/dist/Carousel.js'
@@ -11,11 +12,4 @@ import '@ui5/webcomponents/dist/List.js'
 import '@ui5/webcomponents/dist/ListItem.js'
 import "@ui5/webcomponents/dist/StandardListItem.js"
 
-
-
-
-
-
-
-
-
+setTheme("sap_horizon");
