7238603c19b3f08c79b944eec1707dd31407c3a2
oliver <o.kohl@sap.com>
2023-11-29 16:20:29 +0100

    icon assets added

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import "@ui5/webcomponents-theming/dist/Assets.js"
+import "@ui5/webcomponents-icons/dist/AllIcons.js"
 import '@ui5/webcomponents/dist/Carousel.js'
 import '@ui5/webcomponents/dist/Card.js'
 import '@ui5/webcomponents/dist/CardHeader.js'
 import '@ui5/webcomponents/dist/List.js'
 import '@ui5/webcomponents/dist/ListItem.js'
+import "@ui5/webcomponents/dist/StandardListItem.js"
+
+
+
+
+
+
 
 
 
