48969ac360f372882065315b160de627bce80805
oliver <o.kohl@sap.com>
2023-11-29 15:09:46 +0100

    card added

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import '@ui5/webcomponents/dist/Carousel.js'
+import '@ui5/webcomponents/dist/Card.js'
+import '@ui5/webcomponents/dist/CardHeader.js'
+import '@ui5/webcomponents/dist/List.js'
+import '@ui5/webcomponents/dist/ListItem.js'
+
 
 
